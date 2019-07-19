b0414f70fa2c6121caf052676678cedf4389a740
Trang Nguyen <ttn2127@columbia.edu>
2019-07-19 13:22:45 -0400

    added component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from "react";
+import Card from "./components/card";
+import styled from 'styled-components';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +15,7 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [warsObj, setWarsObj] = useState();
+  const [warsObj, setWarsObj] = useState([]);
   useEffect(()=>{
     axios.get('https://swapi.co/api/people/')
       .then(file =>{
@@ -24,16 +28,39 @@ const App = () => {
 
   },[])
 
-  if(warsObj){
-    console.log(warsObj);
-  }
+  console.log("warsObj", warsObj);
+
+  const AlignCard = styled.div`
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    border: 4px solid blue;
+
+`;
+ 
+
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <AlignCard>
+        {  warsObj.map((element,index) => <Card 
+              key={index}
+              name={element.name} 
+              birth={element.birth_year}
+              eye={element.eye_color}
+              height={element.height}
+              hair={element.hair}
+              home={element.home}
+              species={element.species}
+              
+              />)
+        }
+      </AlignCard>
     </div>
   );
+
 }
 
 export default App;
--- starwars/src/components/card.js
+++ starwars/src/components/card.js
@@ -1,1 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CardDiv = styled.div`
+    width: 45%;
+    margin: auto;
+    margin-top;: 1%;
+`;
+
+const Info = styled.div`
+    border: 1px solid red;
+`;
+
+const Name = styled.h2`
+    background-color: black;
+    color: white;
+    font-size: 2.5rem;
+`;
+
+
+const Card = ({name, birth, eye, height, hair, home, species})=>{
+
+    return(
+        
+        <CardDiv>
+            <Name>Name: {name}</Name>
+            <Info>
+                <h2>Birth Year: {birth}</h2>
+                <h2>Eye: {eye}</h2>
+                <h2>Height: {height}</h2>
+                <h2>Hair Color: {hair}</h2>
+                <h2>Home: {home}</h2>
+                <h2>species: {species}</h2>
+            </Info>
+        </CardDiv>
+
+    )
+
+
+}
+
+export default Card;
